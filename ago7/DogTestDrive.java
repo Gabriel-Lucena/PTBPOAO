5b3b338b34a92fe99261bde0c915e22994df71c6
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2023-08-07 21:49:58 -0300

    Aula de 7 de agosto

---
+++ ago7/DogTestDrive.java
@@ -0,0 +1,17 @@
+package ago7;
+
+public class DogTestDrive {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        
+        d.size = 40;
+
+        d.bark();
+
+        Dog c = d;
+        c = null;
+
+        c.bark();
+
+    }
+}
